e6360793e6c2dd1e50e035ce8e7fdb50f8d3b214
saddeewani1-sudo <saddeewani1@gmail.com>
2025-12-26 23:27:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     bot: {
-        tokens: [
+        tokens: [MTQ1NDIwNzg0MzQxNzk4NTA4NA.GoFE_j.q7BbpF9kwfIja_EQE8G60LlFey4N5V4x5lYv6M
+                
           "YOUR_BOT_TOKEN_HERE",              // Main bot token - Replace with your actual token
         //  "YOUR_SECOND_BOT_TOKEN_HERE",      // 2 bot token - Uncomment and replace to use
         //  "YOUR_THIRD_BOT_TOKEN_HERE",       // 3 bot token - Uncomment and replace to use
@@ -13,13 +14,13 @@ module.exports = {
         activity: {
             name: 'Wick Studio', // رسالة حالة البوت
             type: 'WATCHING', // PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
-            status: 'dnd' // online, idle, dnd, invisible
+            status: 'online' // online, idle, dnd, invisible
         }
     },
     server: {
-        guildId: 'ID',         // ايدي السيرفر
-        broadcastRoleId: 'ID', // ايدي الرول اللي يستخدم عليها البوت
-        reportChannelId: 'ID' // ايدي الروم اللي يرسل فيه البوت التقارير
+        guildId: '1454135377551163716',         // ايدي السيرفر
+        broadcastRoleId: '1454135558996758840', // ايدي الرول اللي يستخدم عليها البوت
+        reportChannelId: '1454204761598460107' // ايدي الروم اللي يرسل فيه البوت التقارير
     },
     broadcast: {
         cooldownTime: 1000, // لا تلعب فيها
@@ -34,4 +35,5 @@ module.exports = {
         error: '#ED4245',      // Red (used for errors)
         neutral: '#5D5D5D'     // Gray (used for neutral messages)
     }
+
 };
