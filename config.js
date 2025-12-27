ecd0356904db249c9b2462c550dce165157c2618
saddeewani1-sudo <saddeewani1@gmail.com>
2025-12-27 11:28:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,15 +11,15 @@ module.exports = {
         defaultLanguage: 'ar', // ar | en لغة البوت
         
         activity: {
-            name: 'Wick Studio', // رسالة حالة البوت
+            name: 'sqr', // رسالة حالة البوت
             type: 'WATCHING', // PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
-            status: 'dnd' // online, idle, dnd, invisible
+            status: 'online' // online, idle, dnd, invisible
         }
     },
     server: {
-        guildId: 'ID',         // ايدي السيرفر
-        broadcastRoleId: 'ID', // ايدي الرول اللي يستخدم عليها البوت
-        reportChannelId: 'ID' // ايدي الروم اللي يرسل فيه البوت التقارير
+        guildId: '1454135377551163716 ',         // ايدي السيرفر
+        broadcastRoleId: '1454135594614788147', // ايدي الرول اللي يستخدم عليها البوت
+        reportChannelId: '1454204761598460107' // ايدي الروم اللي يرسل فيه البوت التقارير
     },
     broadcast: {
         cooldownTime: 1000, // لا تلعب فيها
@@ -34,4 +34,5 @@ module.exports = {
         error: '#ED4245',      // Red (used for errors)
         neutral: '#5D5D5D'     // Gray (used for neutral messages)
     }
+
 };
